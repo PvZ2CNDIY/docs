1f232981172e5b2bff7b11edef3992537b6db682
ji233 <27987772@qq.com>
2024-11-21 19:58:05 +0800

    add: 增加了评论功能

---
+++ .vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="margin-top: 24px">
+        <Giscus
+            id="comments"
+            repo="PvZ2CNDIY/docs"
+            repoid="R_kgDONSsPmw"
+            category="Announcements"
+            categoryid="DIC_kwDONSsPm84Ckeai"
+            mapping="pathname"
+            term="留下你的意见!"
+            reactionsenabled="1"
+            emitmetadata="0"
+            inputposition="top"
+            loading="lazy"
+            :theme="isDark ? 'dark' : 'light'"
+            :key="route.path"
+        ></Giscus>
+    </div>
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue';
+import { useRoute, useData } from 'vitepress';
+
+const route = useRoute();
+const { isDark } = useData();
+</script>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,15 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import GiscusComment from './components/GiscusComment.vue';
+
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'doc-after': () => h(GiscusComment),
     })
   },
   enhanceApp({ app, router, siteData }) {
